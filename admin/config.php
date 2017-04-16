040a4bdfc94578dc0366e2fee6109901798b84f1
Daniel <daniel.statera@gmail.com>
2017-04-16 08:31:35 +0300

    Установлена чистая версия OpenCart. Версия ocStore 2.3.0.2

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dneprkrovlya.testfl.dp.ua/admin/');
+define('HTTP_CATALOG', 'http://dneprkrovlya.testfl.dp.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dneprkrovlya.testfl.dp.ua/admin/');
+define('HTTPS_CATALOG', 'http://dneprkrovlya.testfl.dp.ua/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/admin/');
+define('DIR_SYSTEM', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/system/');
+define('DIR_IMAGE', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/image/');
+define('DIR_LANGUAGE', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/admin/language/');
+define('DIR_TEMPLATE', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/admin/view/template/');
+define('DIR_CONFIG', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/system/config/');
+define('DIR_CACHE', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/system/storage/download/');
+define('DIR_LOGS', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/system/storage/upload/');
+define('DIR_CATALOG', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'dneprkrovlya_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dneprkrovlya.testfl.dp.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dneprkrovlya.testfl.dp.ua/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/catalog/');
+define('DIR_SYSTEM', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/system/');
+define('DIR_IMAGE', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/image/');
+define('DIR_LANGUAGE', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/catalog/language/');
+define('DIR_TEMPLATE', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/system/config/');
+define('DIR_CACHE', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/system/storage/download/');
+define('DIR_LOGS', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OpenServer/domains/dneprkrovlya.testfl.dp.ua/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'dneprkrovlya_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
